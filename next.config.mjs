6a9bf6a89f66ea85aceb1e4ad1b27db17f8e0dfb
munibaweb123 <munibavirtualuni@gmail.com>
2024-11-08 20:42:58 +0500

    random user

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns:[
+        {
+          protocol: "https",
+          hostname: "randomuser.me",
+          port: ""
+        }
+      ]
+    }
+  };
+  
+  export default nextConfig;
